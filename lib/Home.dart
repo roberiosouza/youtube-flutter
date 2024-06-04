b042b202db6e253bcf36f664fb1045be66b28a16
Robério <rroberiosouza@gmail.com>
2024-06-04 20:30:18 -0300

    Montando o layout, adicionado botões de ação e logo.

--- lib/Home.dart
+++ lib/Home.dart
@@ -10,6 +10,18 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          "images/youtube.png",
+          height: 20,
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.videocam)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.account_circle))
+        ],
+      ),
+    );
   }
 }
